6090be8092470692543c3c738871240f6b27af19
Geraldopereirads <geraldo.pereirads@hotmail.com>
2023-05-23 12:00:40 -0300

    fix: reapair workspace

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,30 @@
 import express, { Application } from "express";
-import { createProducts, deleteProductsById, findProducts, findProductsById, updateProductsById } from "./logic";
-import { ensureProductMiddlewaresById, ensureProductNamesMiddlewares, ensureProductNamesPatchMiddlewares } from "./middleware";
+import {
+  createProducts,
+  deleteProductsById,
+  findProducts,
+  findProductsById,
+  updateProductsById,
+} from "./logic";
+import {
+  ensureProductMiddlewaresById,
+  ensureProductNamesMiddlewares,
+  ensureProductNamesPatchMiddlewares,
+} from "./middleware";
 
 const app: Application = express();
 app.use(express.json());
 
 app.post("/products", ensureProductNamesMiddlewares, createProducts);
 app.get("/products", findProducts);
-app.get("/products/:id",ensureProductMiddlewaresById, findProductsById);
-app.patch("/products/:id",ensureProductMiddlewaresById,ensureProductNamesPatchMiddlewares, updateProductsById);
-app.delete("/products/:id",ensureProductMiddlewaresById, deleteProductsById);
+app.get("/products/:id", ensureProductMiddlewaresById, findProductsById);
+app.patch(
+  "/products/:id",
+  ensureProductMiddlewaresById,
+  ensureProductNamesPatchMiddlewares,
+  updateProductsById
+);
+app.delete("/products/:id", ensureProductMiddlewaresById, deleteProductsById);
 
 const PORT: number = 3000;
 const runningMsg: string = `Server running on http://localhost:${PORT}`;
